b5ae24dcfeb0da4f7ab628aec7f6bc9a396ef0f0
Paras Thakur <parasthakur007241@gmail.com>
2023-09-12 20:58:30 +0530

    Part 8 : Pagination

--- src/features/product/ProductAPI.js
+++ src/features/product/ProductAPI.js
@@ -7,29 +7,34 @@ export function fetchAllProducts() {
   resolve({data})}
   );
 }
-export function fetchProductsByFilters(filter,sort) {
+export function fetchProductsByFilters(filter,sort,pagination) {
   //filter: "category":[ " smartphone ","laptops"]
-  // queryString = use after ? for sorting 
+  // queryString = use after ? for sorting
   // todo : on server will support multiple values
   // sort ={sort:"price",_order:"desc"}
+  // pagination = {page:1,_limit:10}_page=1&-limit=10
+  let queryString = "";
 
-  let queryString='';
-  
-  for(let key in filter){
+  for (let key in filter) {
     const categoryValues = filter[key];
     if (categoryValues.length) {
       const lastCategoryValue = categoryValues[categoryValues.length - 1];
       queryString += `${key}=${lastCategoryValue}&`;
     }
-    }
-   for (let key in sort) {
-      queryString += `${key}=${sort[key]}&`;
-    }
- 
+  }
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
+  }
+for (let key in pagination) {
+  queryString += `${key}=${pagination[key]}&`;
+}
   return new Promise(async (resolve) => {
-    const response = await fetch(" http://localhost:8080/products?"+queryString);
-    
+    const response = await fetch(
+      " http://localhost:8080/products?" + queryString
+    );
+
     const data = await response.json();
-    resolve({ data });
+    const totalItems = await response.headers.get('X-Total-Count')
+    resolve({ data:{products:data,totalItems:+totalItems} });
   });
 }
--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -4,6 +4,7 @@ import {
   fetchAllProductsAsync,
   selectAllProducts,
   fetchProductsByFiltersAsync,
+  selectTotalItems,
 } from "../ProductSlice";
 import {
   ChevronLeftIcon,
@@ -21,6 +22,7 @@ import {
 } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
 import { fetchAllProducts } from "../ProductAPI";
+import { ITEMS_PER_PAGE } from "../../../app/constants";
 const sortOptions = [
   { name: "Best Rating", sort: "rating", order: "desc", current: false },
 
@@ -102,35 +104,43 @@ export default function ProductList() {
   const dispatch = useDispatch();
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
   const products = useSelector(selectAllProducts);
+  const totalItems = useSelector(selectTotalItems);
   const [filter, setFilter] = useState({});
-    const [sort, setSort] = useState({});
+  const [sort, setSort] = useState({});
+  const [page, setPage] = useState(1);
+
   const handleFilter = (e, section, option) => {
     const newFilter = { ...filter };
     if (e.target.checked) {
-      if (newFilter[section.id]){
-       newFilter[section.id].push(option.value);}
-       else{
-        newFilter[section.id]=[option.value] 
-       }
+      if (newFilter[section.id]) {
+        newFilter[section.id].push(option.value);
+      } else {
+        newFilter[section.id] = [option.value];
+      }
     } else {
-      const index =  newFilter[section.id].findIndex(el=>el===option.value);
+      const index = newFilter[section.id].findIndex(
+        (el) => el === option.value
+      );
       newFilter[section.id].splice(index, 1);
     }
-     console.log(section.id, option.value);
+    console.log(section.id, option.value);
     setFilter(newFilter);
-
-   
   };
   const handleSort = (e, option) => {
     const sort = { _sort: option.sort, _order: option.order };
     setSort(sort);
-
-
   };
-
+  const handlePage = (page) => {
+    setPage(page);
+  };
   useEffect(() => {
-    dispatch(fetchProductsByFiltersAsync({filter,sort}));
-  }, [dispatch, filter,sort]);
+    const pagination = { _page: page, _limit: ITEMS_PER_PAGE };
+    dispatch(fetchProductsByFiltersAsync({ filter, sort, pagination }));
+  }, [dispatch, filter, sort, page]);
+
+  useEffect(()=>{
+setPage(1)
+  },[totalItems,sort])
   return (
     <div>
       <div>
@@ -231,7 +241,12 @@ export default function ProductList() {
               </section>
               {/* {Product and filter ends here} */}
 
-              <Pagination></Pagination>
+              <Pagination
+                page={page}
+                setPage={setPage}
+                handlePage={handlePage}
+                totalItems={totalItems}
+              ></Pagination>
             </main>
           </div>
         </div>
@@ -421,7 +436,7 @@ function DesktopFilter({ handleFilter }) {
   );
 }
 
-function Pagination() {
+function Pagination({ page, setPage, handlePage, totalItems }) {
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
@@ -441,9 +456,17 @@ function Pagination() {
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700">
-            Showing <span className="font-medium">1</span> to{" "}
-            <span className="font-medium">10</span> of{" "}
-            <span className="font-medium">97</span> results
+            Showing{" "}
+            <span className="font-medium">
+              {(page - 1) * ITEMS_PER_PAGE + 1}
+            </span>{" "}
+            to{" "}
+            <span className="font-medium">
+              {page * ITEMS_PER_PAGE > totalItems
+                ? totalItems
+                : page * ITEMS_PER_PAGE }
+            </span>{" "}
+            of <span className="font-medium">{totalItems}</span> results
           </p>
         </div>
         <div>
@@ -459,25 +482,26 @@ function Pagination() {
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
             </a>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-            <a
-              href="#"
-              aria-current="page"
-              className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              1
-            </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              2
-            </a>
-            <a
-              href="#"
-              className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-            >
-              3
-            </a>
+            {Array.from({ length: Math.ceil(totalItems / ITEMS_PER_PAGE) }).map(
+              (el, index) => (
+                <div
+                  onClick={(e) => handlePage(index + 1)}
+                  aria-current="page"
+                  className={`relative cursor-pointer z-10 inline-flex items-center
+                  ${
+                    index + 1 == page
+                      ? " bg-indigo-600  text-white"
+                      : "text-gray-900 "
+                  } px-4 py-2 text-sm font-semibold
+                     focus:z-20 focus-visible:outline 
+                    focus-visible:outline-2 focus-visible:outline-offset-2
+                     focus-visible:outline-indigo-600`}
+                >
+                  {index + 1}
+                </div>
+              )
+            )}
+
             <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
               ...
             </span>
